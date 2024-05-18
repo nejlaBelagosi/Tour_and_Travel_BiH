e16f8c463c788ef5bae0038fce7770c41ac3ab39
nejlaBelagosi <122165597+nejlaBelagosi@users.noreply.github.com>
2024-05-18 18:33:42 +0200

    landing page of logged user

---
+++ Frontend/tour-and-travel-bih-enduser/src/components/AnimRoutes.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+//import pages
+import Homepage from "../pages/Homepage";
+// import User from "../pages/User";
+// import Destination from "../pages/Destination";
+// import Accounts from "../pages/Accounts";
+// import Payment from "../pages/Payment";
+// import Reservations from "../pages/Reservations";
+// import TourPackage from "../pages/TourPackage";
+
+//import routes route & useLocation
+import {Routes, Route, useLocation} from 'react-router-dom';
+
+
+const AnimRoutes = () => {
+    const location = useLocation();
+return (
+   
+        <Routes key={location.pathname} location={location} >
+            <Route path="/" element ={<Homepage />} />
+            {/* <Route path="/User" element ={<User />} />
+            <Route path="/Destination" element ={<Destination />} />
+            <Route path="/Accounts" element ={<Accounts />} />
+            <Route path="/Payment" element ={<Payment />} />
+            <Route path="/Reservations" element ={<Reservations />} />
+            <Route path="/TourPackage" element ={<TourPackage />} /> */}
+            
+        </Routes>
+
+
+)
+};
+
+export default AnimRoutes;
